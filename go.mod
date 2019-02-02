f11266ee5decddf288e74f6808f0137a61bfa932
nokia_lte <nokia_lte@ctdi.com>
2019-02-01 20:33:22 -0500

    Commiting go mod go.sum and updatd main.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lukusbeaur/Auth_Session_Server
+
+require github.com/gomodule/redigo v2.0.0+incompatible
--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+var cache redis.Conn
+
 func main() {
+	initCache()
+	//"Signin" and "Welcome" are the handlers that we will implement
+	http.HandleFunc("/signin", Signin)
+	http.HandleFunc("/welcome", Welcome)
+
+	//start the server
+	log.Fatal(http.ListenAndServe("8080", nil))
+}
+
+func initCache() {
+	//initialize the redis connection
+	conn, err := redis.DialURL("redis://localhost")
+	if err != nil {
+		panic(err)
+	}
+
+	//assign the connection to the package level
+	cache = conn
+}
+
+func Welcome(w http.ResponseWriter, r *http.Request) {
+
+}
+func Signin(w http.ResponseWriter, r *http.Request) {
 
 }
